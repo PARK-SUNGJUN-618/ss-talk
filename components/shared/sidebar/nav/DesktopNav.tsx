d6620b9bd729e3a6cbb6bf3dd8436b410eae25ef
Sungjun Park <espoir618@gmail.com>
2024-07-21 13:02:55 +0900

    [Jun] Create hooks for adding navigation

---
+++ components/shared/sidebar/nav/DesktopNav.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { useNavigation } from "@/hooks/useNavigation";
+import { UserButton } from "@clerk/nextjs";
+
+const DesktopNav = () => {
+  const paths = useNavigation();
+
+  return (
+    <Card
+      className="hidden lg:flex lg:flex-col lg:justify-between lg:items-center
+        lg:h-full lg:w-16 lg:px-2 lg:py-4"
+    >
+      <nav></nav>
+      <div className="flex flex-col items-center gap-4">
+        <UserButton />
+      </div>
+    </Card>
+  );
+};
+
+export default DesktopNav;
