d60874071a22ec7bb738c50404ff1f1f8de5e2d3
Sungjun Park <espoir618@gmail.com>
2024-07-29 19:16:00 +0900

    [Jun] Edit Add Friend Dialog Component

---
+++ app/(root)/friends/_components/AddFriendDialog.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import { UserPlus } from "lucide-react";
+
+type Props = {};
+
+const addFriendFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "This field can't be empty" })
+    .email("Please enter a valid email"),
+});
+
+const AddFriendDialog = (props: Props) => {
+  const form = useForm<z.infer<typeof addFriendFormSchema>>({
+    resolver: zodResolver(addFriendFormSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  return (
+    <Dialog>
+      <Tooltip>
+        <TooltipTrigger>
+          <Button size="icon" variant="outline">
+            <DialogTrigger>
+              <UserPlus />
+            </DialogTrigger>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Add Friend</p>
+        </TooltipContent>
+      </Tooltip>
+    </Dialog>
+  );
+};
+
+export default AddFriendDialog;
